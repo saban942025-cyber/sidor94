9d819bdb26412e8023b7039417d34ad5b2a3b417
saban942025-cyber <saban942025@gmail.com>
2025-11-10 06:34:33 +0200

    Update OneSignalSDKWorker.js

--- sidor/OneSignalSDKWorker.js
+++ sidor/OneSignalSDKWorker.js
@@ -1,1 +1,6 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+/*
+  This file is imported by the OneSignal SDK.
+  It's the standard service worker file required for push notifications.
+*/
+
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
