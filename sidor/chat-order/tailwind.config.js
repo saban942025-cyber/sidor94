353a9af5a73f8cba5655c9aceb07e7d575ae742a
saban942025-cyber <saban942025@gmail.com>
2025-11-18 08:53:47 +0200

    Create tailwind.config.js

---
+++ sidor/chat-order/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // סורק את כל קבצי ה-React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
