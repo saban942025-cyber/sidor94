74efe4312a5c87a39270a54292ca977df45fea69
saban942025-cyber <saban942025@gmail.com>
2025-11-21 07:56:43 +0200

    Update firebase.js

--- sidor/chat-order/firebase.js
+++ sidor/chat-order/firebase.js
@@ -1,11 +1,12 @@
 // קובץ חיבור מרכזי ל-Firebase
 // זה מחליף את הבלוקים החוזרים של initializeApp בכל קובץ HTML
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, signInWithCustomToken, signInAnonymously } from "firebase/auth"; // Added imports
+import { getFirestore, doc, setDoc, getDoc, collection, query, onSnapshot, addDoc, updateDoc, deleteDoc } from "firebase/firestore"; // Added imports
 import { getStorage } from "firebase/storage";
 
 // הקונפיגורציה נלקחת מהמשתנים שהגדרת בקבצים הקודמים
+// In a real app, use environment variables (import.meta.env.VITE_FIREBASE_API_KEY etc.)
 const firebaseConfig = {
   apiKey: "AIzaSyDq0oVwS6zbEfsgrYBRkeBq80dDUKMedzo", 
   authDomain: "saban94-78949.firebaseapp.com", 
@@ -20,5 +21,21 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// Exporting common functions to be used in other files if needed, 
+// though direct import from firebase/firestore is preferred in modern modular SDK
+export { 
+    signInWithCustomToken, 
+    signInAnonymously,
+    doc, 
+    setDoc, 
+    getDoc, 
+    collection, 
+    query, 
+    onSnapshot, 
+    addDoc, 
+    updateDoc, 
+    deleteDoc 
+};
 
 export default app;
