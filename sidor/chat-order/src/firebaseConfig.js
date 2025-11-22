930cdf109b6a575e86ad019434717b93b81e07c1
saban942025-cyber <saban942025@gmail.com>
2025-11-22 05:06:44 +0200

    Create firebaseConfig.js

---
+++ sidor/chat-order/src/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDq0oVwS6zbEfsgrYBRkeBq80dDUKMedzo", 
+  authDomain: "saban94-78949.firebaseapp.com", 
+  projectId: "saban94-78949", 
+  storageBucket: "saban94-78949.firebasestorage.app", 
+  messagingSenderId: "41553157903", 
+  appId: "1:41553157903:web:cc33d252cff023be97a87a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export { signInAnonymously, doc, getDoc };
+
+export default app;
